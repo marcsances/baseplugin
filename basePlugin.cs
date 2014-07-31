0240a9f9c05a55b809a27109be8369e4a4db3d53
MSS Software & Services <msssoftware@outlook.com>
2014-07-31 12:57:07 +0200

    Initial push

---
+++ basePlugin.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+namespace fPlayer_2
+{
+    public class basePlugin : IPlugin
+    {
+        public string name = "basePlugin";
+        public Control getGUI()
+        {
+            return new pluginUI();
+        }
+    }
+}
